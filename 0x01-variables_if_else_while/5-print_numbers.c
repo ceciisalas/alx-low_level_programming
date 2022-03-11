028020fa48235541a3a93a169eb73736eab00484
ceciisalas <cecizs1@hotmail.com>
2022-03-11 14:08:27 -0800

    Single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Print all sigle digit numbers of base 10
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	for (i = 0; i = 0 < 9; i++)
+	{
+		printf(i);
+	}
+	return (0);
+}
