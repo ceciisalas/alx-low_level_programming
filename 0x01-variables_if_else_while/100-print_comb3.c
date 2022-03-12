1e8f56f7a0f38578e34f4d6b88aa3c0ae31384a9
ceciisalas <cecizs1@hotmail.com>
2022-03-11 16:39:04 -0800

    2 digit combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,16 +9,17 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a <= 9; a++)
+	for (a = '0'; a <= '9'; a++)
 	{
 		for (b = '0'; b <= '9'; b++)
 		{
-			if (!(a == a) && a < b)
+			if (a < b)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
 
-				if (!(a == 8 && b == 9))
+				putchar(a);
+				putchar(b);
+
+				if (a != '8' || (a == '8' && b != '9'))
 				{
 					putchar(',');
 					putchar(' ');
