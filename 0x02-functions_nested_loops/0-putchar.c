ae62aff34965f779a21ead260cca34bcf0b4160b
ceciisalas <cecizs@hotmail.com>
2022-03-15 19:27:44 +0000

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - prints my name
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('C');
+	_putchar('E');
+	_putchar('C');
+	_putchar('I');
+	_putchar('\n');
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0 FILE - Write a program that prints _putchar, followed my a new line.
