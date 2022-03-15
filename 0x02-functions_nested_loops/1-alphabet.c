a9a1271de467e192e28a46aeaeeb5c57a5342d3d
ceciisalas <cecizs@hotmail.com>
2022-03-15 21:09:35 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * main - Prints the alphabet
+ * print_alphabet - Prints the alphabet
  *
  * Return: Always 0.
  */
-int main(void)
+void print_alphabet(void)
 {
 	char alphabet = 'a';
 
