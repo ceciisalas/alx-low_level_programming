0ab889a3cc54020d6cba7488fde1c56c4b129280
ceciisalas <cecizs@hotmail.com>
2022-03-15 21:05:50 +0000

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Prints the alphabet
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char alphabet = 'a';
+
+	while (alphabet <= 'z')
+	{
+		_putchar(alphabet);
+		alphabet++;
+	}
+	_putchar('\n');
+	return (0);
+}
