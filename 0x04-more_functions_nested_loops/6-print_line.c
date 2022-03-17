0c82ae39d23e0f12b661bca599c68736809c15e9
ceciisalas <cecizs1@hotmail.com>
2022-03-17 21:26:23 +0000

    line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_line - Print a straight line
+ * @n: number of - characters
+ * Return: 0
+ */
+void print_line(int n)
+{
+	for (i = 0; i < 65; i++)
+	{
+		_putchar('_´);
+	}
+	_putchar('\n');
+}
