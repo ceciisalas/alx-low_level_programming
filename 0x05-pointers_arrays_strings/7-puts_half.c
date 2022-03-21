dc9f6f7d602033014b4c1d220244f15fa3ac4e0d
ceciisalas <cecizs1@hotmail.com>
2022-03-21 22:32:07 +0000

    Half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts_half - Print half of a string
+ * @str: half of a string
+ *
+ * Return_ Always 0
+ */
+void puts_half(char *str)
+{
+	int s = strlen(s);
+
+	for (int i = 0; i < size; i++)
+	{
+		cout << *(str + i);
+	}
+}
