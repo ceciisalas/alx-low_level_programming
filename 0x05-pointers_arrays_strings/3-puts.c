a92668b603c94ad6cee9f611870a06a6b03ed257
ceciisalas <cecizs1@hotmail.com>
2022-03-21 20:49:06 +0000

    Print a string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - Prints a string, followed by a new line, to stdout
+ * @str: string
+ *
+ * Return: Always 0
+ */
+void _puts(char *str)
+{
+	*str[4] = "hola";
+
+	puts(string);
+	return (0);
+}
