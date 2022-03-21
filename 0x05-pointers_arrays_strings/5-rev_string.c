67f14d8e57cdb3a8402a2dca3e2c4650faafd106
ceciisalas <cecizs1@hotmail.com>
2022-03-21 21:48:42 +0000

    reverse string 2

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * rev_string - Reverse a string
+ * @s: string
+ *
+ * Return: Always 0
+ */
+void rev_string(char *s)
+{
+	char *a = s;
+	char str[100];
+	short b = 0;
+
+	while (*s != '\0')
+	{
+		str[b] = *s;
+		s++;
+		b++;
+	}
+	b = 0;
+
+	while (s > a)
+	{
+		s--;
+		*s = n[b];
+		c++;
+	}
+}
