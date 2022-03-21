66c1d2dc09d48b0d1c82fcdf1fc7e418bdf93c4d
ceciisalas <cecizs1@hotmail.com>
2022-03-21 20:39:01 +0000

    String length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,10 +7,12 @@
  */
 int _strlen(char *s)
 {
-	int length;
-	char array[4] = "hola";
+	int length = 0;
 
-	length = strlen(array);
-	_putchar("\string length  = %d \n", length);
-	return (0);
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
 }
