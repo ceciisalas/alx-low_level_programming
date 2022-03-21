deed59939fc6fb4d95e60ffac7d9b703043806a9
ceciisalas <cecizs1@hotmail.com>
2022-03-21 21:16:48 +0000

    Reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_rev - Print a string, in reverse
+ * @s: string
+ *
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int i, count, j;
+
+	for (i = 0; i < count; i++)
+	{
+		rev[i] = *s[j];
+		j--;
+	}
+	_putchar('\n');
+}
