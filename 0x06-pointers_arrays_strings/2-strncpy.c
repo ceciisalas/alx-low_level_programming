0c6920b99498cd21972f3058584e1e0dd679f368
ceciisalas <cecizs1@hotmail.com>
2022-03-23 20:59:25 +0000

    Copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_strncpy - Copies a string
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
