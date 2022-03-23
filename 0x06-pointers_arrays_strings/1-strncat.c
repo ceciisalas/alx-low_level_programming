320d86561d037013114aac9463da36e738c953cc
ceciisalas <cecizs1@hotmail.com>
2022-03-23 20:31:22 +0000

    2 strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strncpy - Concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	i = 0;
+	while (i < n && src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
