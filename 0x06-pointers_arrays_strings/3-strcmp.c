72b9054db0345d9329245bdde063261da6fd39ae
ceciisalas <cecizs1@hotmail.com>
2022-03-23 21:20:49 +0000

    compare a string

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcmp - Compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return: 1 or 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
