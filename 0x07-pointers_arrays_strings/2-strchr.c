98084b892486de6bea8d28986d1e3c81549beebf
ceciisalas <cecizs1@hotmail.com>
2022-03-28 21:02:15 +0000

    locates a char

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,14 +1,25 @@
 #include "main.h"
+
 /**
- * *_strchr - Locates a character.
- * @s: string
+ * *_strchr - Localtes a string
+ * @s: pointer to char
  * @c: character
- *
  * Return: NULL
- */
+*/
+
 char *_strchr(char *s, char c)
 {
-	for (; *s != '\0' && *s != c; ++s)
-		;
-	return (*s == c ? (char *) s : NULL);
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+return (0);
 }
