8cabeb192fb284880892d345ab8c9fce8fdaf7a6
ceciisalas <cecizs1@hotmail.com>
2022-04-05 18:20:53 +0000

    123

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *create_array - Create an array of chars
+ * and initializes it with a specific char.
+ * @size: size of array
+ * @c: character
+ * Return: Null if size = 0
+ * Return: a pointer to the array, or NULL if it fails
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i = 0;
+	char *arr;
+
+	arr = (char *) malloc(size);
+	if (size == 0 || arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,7 @@
+File 0 - Creates an array of chars.
+*File 1 - Function that returns a pointer to a newly allocated space in memory,which contains a copy of the string give ad a parameter.
+*File 2 - Function that concatenates two strings
+*File 3 - Function that returns a pointer toa 2 dimensional array of integers
+*File 4 - Function that frees a 2 dimensional grid previosuly created by your allloc_grid function.
+*File 5 - Function that concatenates all the arguments of your program
+*File 6 - Function that splits a string into words.
