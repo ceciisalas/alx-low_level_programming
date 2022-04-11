cd6ae67c5b5d0fef8cec8aea1ecccdd89bea9681
ceciisalas <cecizs1@hotmail.com>
2022-04-11 20:51:08 +0000

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,14 @@
 #ifndef DOG_H
-#define DOG.H
+#define DOG_H
+/**
+ * struct dog - New struct dog
+ * @name: char
+ * @age: float
+ * @owner: second char
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
