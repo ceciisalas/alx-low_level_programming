7d72e6d280f95b07cbf6f122f272abf629be2ada
ceciisalas <cecizs1@hotmail.com>
2022-04-13 12:07:18 +0000

    name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,10 +1,11 @@
+#include <stdio.h>
 #include "function_pointers.h"
 /**
- * print_name - Print a name
+ * print_name - print a name
  * @name: name
- * @f: function
+ * @f: functions
  */
-void print_name(char *name, void (*f)(char*))
+void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
 	{
