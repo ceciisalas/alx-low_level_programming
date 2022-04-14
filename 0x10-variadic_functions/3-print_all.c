ecc04621f6b67ec5605b9e941d67a5a54010c829
ceciisalas <cecizs1@hotmail.com>
2022-04-14 16:48:01 +0000

    printall

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,101 +1,51 @@
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include "variadic_functions.h"
 
 /**
- * print_char - function that prints character
- * @argptr: argument list pointer to character to be printed
- */
-
-void print_char(va_list argptr)
-{
-	char character;
-
-	character = va_arg(argptr, int);
-	printf("%c", character);
-}
-
-/**
- * print_int - function that prints integer
- * @argptr: argument list pointer to integer to be printed
- */
-
-void print_int(va_list argptr)
-{
-	int num_i;
-
-	num_i = va_arg(argptr, int);
-	printf("%i", num_i);
-}
-
-/**
- * print_float - function that prints a float
- * @argptr: argument list pointer to float to be printed
- */
-
-void print_float(va_list argptr)
-{
-	float num_f;
-
-	num_f = va_arg(argptr, double);
-	printf("%f", num_f);
-}
-
-/**
- * print_string - function that prints string
- * @argptr: argument list pointer to string to be printed
- */
-
-void print_string(va_list argptr)
-{
-	char *str;
-
-	str = va_arg(argptr, char*);
-
-	if (str == NULL)
-	{
-		printf("(nil)");
-		return;
-	}
-	printf("%s", str);
-}
-
-/**
- * print_all - function that prints anything
- * @format: list of types of arguments passed to the function
- * @...: number of arguments
- * Return: Nothing
- */
-
+* print_all - print char, integer, float and string
+* @format: format
+*/
 void print_all(const char * const format, ...)
 {
-	va_list arguments;
-	int index, arg_len;
-	char *separator = "";
+	va_list list;
+	unsigned int j = 0, start = 0;
+	char *p;
 
-	prints_t fmt_list[] = {
-		{"c", print_char},
-		{"i", print_int},
-		{"f", print_float},
-		{"s", print_string}
-	};
-	va_start(arguments, format);
-	index = 0;
-	while (format && (*(format + index)))
+	va_start(list, format);
+	while (format && format[j] != '\0')
 	{
-		arg_len = 0;
-		while (arg_len < 4 && (*(format + index) != *(fmt_list[arg_len].notation)))
-		{
-			arg_len++;
-		}
-		if (arg_len < 4)
-		{
-			printf("%s", separator);
-			fmt_list[arg_len].print_dType(arguments);
-			separator = ", ";
-		}
-		index++;
+		switch (format[j])
+		{ case 'c':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%c", va_arg(list, int));
+			break;
+			case 'i':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%i", va_arg(list, int));
+			break;
+		case 'f':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			printf("%f", va_arg(list, double));
+			break;
+		case's':
+			switch (start)
+			{ case 1: printf(", "); }
+			start = 1;
+			p = va_arg(list, char*);
+			if (p)
+			{ printf("%s", p);
+			break; }
+			printf("%p", p);
+			break; }
+		j++;
 	}
 	printf("\n");
-	va_end(arguments);
+	va_end(list);
 }
