5c762db1a45490e082d87f5b5f3484cbd0a7df74
ceciisalas <cecizs1@hotmail.com>
2022-04-26 21:10:57 +0000

    insert node

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,13 +1,14 @@
 #include "lists.h"
 #include <stdlib.h>
+
 /**
- * insert_nodeint_at_index - Insert a new node at a given positition
- * *idx: Index of the list where the new node should be added
- * index starts at 0
- * Return: the address of the new node, or NULL if it failed
- * If it is no possible to add the new node at index idx, do not add the new
- * node and return NULL
- */
+ * insert_nodeint_at_index - Insert a new node at a given positiion.
+ * @head: First node address.
+ * @idx: Position of the new node to be inserted in.
+ * @n: Data of the new node.
+ * Return: Address of the new node.
+ **/
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new_node, *temp;
@@ -17,23 +18,23 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		return (NULL);
 	if (idx != 0)
 	{
-		temp = *head;
-		for (; i < idx - 1 && temp != NULL; i++)
-			temp = temp->next;
-		if (temp == NULL)
-			return (NULL);
+	temp = *head;
+	for (; i < idx - 1 && temp != NULL; i++)
+		temp = temp->next;
+	if (temp == NULL)
+		return (NULL);
 	}
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-	new_node-> = n;
+	new_node->n = n;
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
-	new_node->next; = temp->next;
+	new_node->next = temp->next;
 	temp->next = new_node;
 	return (new_node);
 }
