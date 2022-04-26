b95da4abc915818745d4350330ca46f0368649ef
ceciisalas <cecizs1@hotmail.com>
2022-04-26 20:21:38 +0000

    free list 2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
+
 /**
- * free_listint2 - Free list
- * @head: Head of the list
- */
-void free_listint2(listint_t *head)
+ * free_listint2 - Frees a list.
+ * @head: Address of the first node of a list.
+ **/
+
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
