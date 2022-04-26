36cec4364f09161c62927d4c233b1a681ae19a96
ceciisalas <cecizs1@hotmail.com>
2022-04-26 14:47:59 +0000

    printList

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_listint - Print elements of a singly linked list
+ * @h: Pointer to a list
+ * Return: n of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	const listint_t *temp;
+	unsigned int i = 0;
+
+	temp = h;
+	while (temp)
+	{
+		printf("%d\n", temp->n);
+		i++;
+		temp = temp->next;
+	}
+	return (i);
+}
