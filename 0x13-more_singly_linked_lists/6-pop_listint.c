2b07ddacfae655ff674dc400e8a5d28c9801a47f
ceciisalas <cecizs1@hotmail.com>
2022-04-26 20:26:28 +0000

    delete first element

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * pop_listint - Delete the first element of a singly linked list
+ * @head: Pointer to a list
+ * Return: 0;
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int data;
+
+	if (*head == NULL)
+		return (0);
+	temp = *head;
+	*head = temp->next;
+	data = temp->n;
+	free(temp);
+	return(data);
+}
