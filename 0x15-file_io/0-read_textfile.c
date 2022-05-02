19be8a91062e55d4ac9370023a041449ef3a1c51
ceciisalas <96299262+ceciisalas@users.noreply.github.com>
2022-05-02 16:13:39 -0500

    Update 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,38 +1,37 @@
 #include "main.h"
 /**
- * read_textfile - Read a text file and print it to the STDOUT
- * @filename: name of the file
- * @letters: number of letters
- * Return: The number of letters to be printed
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
+ * Return: the number of letters printed, or 0 if it failed
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int txt;
-	int i, j;
-	char *fm;
-
+	int fd;
+	int i, y;
+	char *buf;
 	if (!filename)
 		return (0);
-	txt = open(filename, O_RDONLY);
-	if (txt < 0)
+	fd = open(filename, O_RDONLY);
+	if (fd < 0)
 		return (0);
-	fm = malloc(sizeof(char) * letters);
-	if (!fm)
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
 		return (0);
-	i = read(txt, fm, letters);
+	i = read(fd, buf, letters);
 	if (i < 0)
 	{
-		free(fm);
+		free(buf);
 		return (0);
 	}
-	fm[i] = '\0';
-	close(txt);
-	j = write(STDOUT_FILENO, fm, i);
-	if (j < 0)
+	buf[i] = '\0';
+	close(fd);
+	y = write(STDOUT_FILENO, buf, i);
+	if (y < 0)
 	{
-		free(fm);
+		free(buf);
 		return (0);
 	}
-	free(fm);
-	return (j);
+	free(buf);
+	return (y);
 }
