c3faff708743dfa8cfd1bed75a2b08b55fda8bac
ceciisalas <cecizs1@hotmail.com>
2022-06-09 21:55:48 +0000

    free lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_dlistint - free double linked list
+ * @head: head of the list
+ *
+ */
+void free_dlistint(dlistint_t *head)
+{
+	while (head != NULL)
+	{
+		dlistint_t *next = head->next;
+		free(head);
+		head = next;
+	}
+}
