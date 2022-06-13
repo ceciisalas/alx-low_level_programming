b80b2c9b97f9ae24ad1afc475ed2e38e574d3121
ceciisalas <cecizs1@hotmail.com>
2022-06-13 23:01:29 +0000

    dynamic library

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - Computes the absolute value of an integer.
+ *
+ * @a: The integer to be computed
+ *
+ * Return: Absolute value
+ */
+int _abs(int a)
+{
+	if (a <  0)
+	{
+		return (a * -1);
+	}
+	return (a);
+}
