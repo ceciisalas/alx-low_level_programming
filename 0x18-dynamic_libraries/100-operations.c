a0a3f6a3fc437e0383449f5990cf1ac6eb6530fe
ceciisalas <cecizs1@hotmail.com>
2022-06-13 23:22:08 +0000

    dll operations

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,59 @@
+/**
+ * add -sum
+ * @a: number
+ * @b: number
+ * Return: Result
+ */
+
+int add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * sub - substraction
+ * @a: number
+ * @b: number
+ * Return: Result
+ */
+
+int sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * mul - multiplication
+ * @a: number
+ * @b: number
+ * Return: Result
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * div - division
+ * @a: number
+ * @b: number
+ * Return: Result
+ */
+
+int div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * mod - modulo
+ * @a: number
+ * @b: number
+ * Return: Result
+ */
+
+int mod(int a, int b)
+{
+return (a % b);
+}
