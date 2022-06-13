197817c119840d67bf77c205cac37b8b7bdca660
ceciisalas <cecizs1@hotmail.com>
2022-06-13 23:32:12 +0000

    make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-wget https://github.com/ceciisalas/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so -P ../
-export LD_PRELOAD=../inject.so
+#!/bin/bash
+wget -P .. https://raw.githubusercontent.com/ceciisalas/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD="$PWD/../libgiga.so"
